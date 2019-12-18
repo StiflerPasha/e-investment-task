f4f987ec20cde0d917ab291a801e82d81ad0f2b8
StiflerPasha <dvoynospavel@gmail.com>
2019-12-18 15:21:09 +0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app app>
+    <v-app-bar app>
+
+      <v-spacer/>
+      <div v-if="$auth.loggedIn">
+        {{$auth.user.email}}
+        <v-btn text>Logout</v-btn>
+      </div>
+
+      <div v-else>
+        <v-btn text to="/login">Login</v-btn>
+        <v-btn text to="/register">Register</v-btn>
+      </div>
+
+    </v-app-bar>
+    <h1>Title</h1>
+  </v-app>
+</template>
+
+<script>
+  export default {};
+</script>
